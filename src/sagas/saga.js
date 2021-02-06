f4f834368a276f8b63d8fbe2b9eef363452eedc8
Quiana Berry <berry.quiana@gmail.com>
2021-02-05 23:56:18 -0500

    trial 1

---
+++ src/sagas/saga.js
@@ -0,0 +1,14 @@
+//sagas are implemented ad generator
+//functions that yield objects to the redux-saga middleware
+
+import { delay } from "redux-saga/effects";
+import { takeLatest, put } from "redux-saga/effects";
+
+function* ageUpAsync() {
+	yield delay(4000);
+	yield put({ type: "AGE_UP_ASYNC", value: 1 });
+}
+
+export function* watchAgeUp() {
+	yield takeLatest("AGE_UP", ageUpAsync);
+}
